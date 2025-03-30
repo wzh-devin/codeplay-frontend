2094d902f5e22142afee361ae00e64715e23b805
devin <2460316849@qq.com>
2025-03-30 18:20:43 +0800

    feat: 初始化项目模板

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
-import { useRoute, useRouter } from 'vue-router'
-import useUserStore from '@/store/user.ts'
+import { useRouter } from 'vue-router'
+import useUserStore from './store/user.ts'
 
 // 路由鉴权处理
 const router = useRouter()
-const route = useRoute()
 
 const userStore = useUserStore()
 
 // 路由守卫
 router.beforeEach((to, from, next) => {
+  console.log(from)
   // 判断登录用户是否有权限
   if (to.meta?.authorize === 'admin') {
     if (userStore.loginUser.role !== 'admin') {
